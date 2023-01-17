59f26207aa0d9d8abedf7df35eab6feeac62fd8e
Ted <ted@Teds-MacBook-Pro-2.local>
2023-01-17 14:32:18 -0800

    Tests Update

---
+++ app/src/test/java/edu/ucsd/cse110/lab2/BetterMainActivityTest.java
@@ -0,0 +1,36 @@
+package edu.ucsd.cse110.lab2;
+
+import static org.junit.Assert.assertEquals;
+
+import android.widget.TextView;
+
+import androidx.lifecycle.Lifecycle;
+import androidx.test.core.app.ActivityScenario;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.robolectric.RobolectricTestRunner;
+
+@RunWith(RobolectricTestRunner.class)
+
+public class BetterMainActivityTest {
+    @Test
+    public void test_one_plus_one_equals_two() {
+        var scenario = ActivityScenario.launch(MainActivity.class);
+        scenario.moveToState(Lifecycle.State.CREATED);
+        scenario.moveToState(Lifecycle.State.STARTED);
+
+        scenario.onActivity(activity -> {
+            TextView one = activity.findViewById(R.id.btn_one);
+            TextView plus = activity.findViewById(R.id.btn_plus);
+            TextView equals = activity.findViewById(R.id.btn_equals);
+            TextView display = activity.findViewById(R.id.display);
+
+            one.performClick();
+            plus.performClick();
+            one.performClick();
+            equals.performClick();
+            assertEquals(2, display.getText());
+        });
+    }
+}
